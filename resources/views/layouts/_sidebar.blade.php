0c2f18100fd632f6895ff2dae96da08e91e0241d
Niels Drost <nielsdrost7@gmail.com>
2018-10-17 22:42:08 +0200

    clean laravel 5.5 + Coreui (with grunt)

---
+++ resources/views/layouts/_sidebar.blade.php
@@ -0,0 +1,86 @@
+<div class="sidebar">
+    <button class="sidebar-minimizer brand-minimizer" type="button"></button>
+    <nav class="sidebar-nav">
+        <ul class="nav">
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <i class="nav-icon fa fa-dashboard"></i> <span>@lang('ip.dashboard')</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <i class="nav-icon fa fa-users"></i> <span>@lang('ip.clients')</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <i class="nav-icon fa fa-file-text-o"></i> <span>@lang('ip.quotes')</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link"
+                        href="#">
+                    <i class="nav-icon fa fa-file-text"></i> <span>@lang('ip.invoices')</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <i class="nav-icon fa fa-refresh"></i> <span>@lang('ip.recurring_invoices')</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <i class="nav-icon fa fa-credit-card"></i> <span>@lang('ip.payments')</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <i class="nav-icon fa fa-bank"></i> <span>@lang('ip.expenses')</span>
+                </a>
+            </li>
+            <li class="nav-item nav-dropdown">
+                <a class="nav-link nav-dropdown-toggle" href="#">
+                    <i class="nav-icon fa fa-bar-chart-o"></i> <span>@lang('ip.reports')</span>
+                </a>
+                <ul class="nav-dropdown-items">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            @lang('ip.client_statement')
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            @lang('ip.expense_list')
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            @lang('ip.item_sales')
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            @lang('ip.payments_collected')
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            @lang('ip.profit_and_loss')
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            @lang('ip.revenue_by_client')
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            @lang('ip.tax_summary')
+                        </a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </nav>
+    <button class="sidebar-minimizer brand-minimizer" type="button"></button>
+</div>
